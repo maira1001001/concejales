caab53995ce52fd50842338e9b2bad2dd82bf46c
maira1001001 <maira.diaz@protonmail.com>
2016-06-26 00:50:17 -0300

    add error spellings

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,4 @@
-class Particiaption < ActiveRecord::Base
+class Participation < ActiveRecord::Base
   belongs_to :person
   belongs_to :term, autosave: true
   has_one :collaborator, class: 'Participation', foreign_key: 'collaborator_id'
