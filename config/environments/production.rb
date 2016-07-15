0b25a6f11e34dafa6b29e6f68b8cd7904950d6f9
maira1001001 <maira.diaz@protonmail.com>
2016-07-15 01:56:07 -0300

    change gmail configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'concejales.herokuapp.com' port: 80  }
+  config.action_mailer.default_url_options = { host: 'concejales.herokuapp.com'}
 
   config.action_mailer.delivery_method = :smtp
 
