b7d3af7fa1716966312fd466327965442bc16262
maira1001001 <maira.diaz@protonmail.com>
2016-07-15 01:45:56 -0300

    add menu styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
 #------------------------------------------
-    # Mailers settings
+  # Mailers settings
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = Settings.mailer.smtp_settings.to_hash
 
