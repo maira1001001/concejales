6ccafa98ed1f1ff657b7141fbbba6da03bbc5e72
maira1001001 <maira.diaz@protonmail.com>
2016-07-19 19:48:51 -0300

    fix project title

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,10 +1,10 @@
 module ProjectsHelper
 
-  PROJECT_TYPES = { ordinance: 0, decree: 1, resolution: 2, communication: 3, document: 4  }
+  PROJECT_TYPES = [ :ordinance, :decree, :resolution, :communication, :document ]
 
   def project_title(project_type_params)
-    if project_type_params && PROJECT_TYPES.has_value?(project_type_params.to_sym)
-      t("projects.new.#{PROJECT_TYPES[project_type_params.to_sym]}")
+    if project_type_params && PROJECT_TYPES.include?(project_type_params.to_sym)
+      t("projects.new.#{project_type_params}")
     else
       t("projects.new.title")
     end
