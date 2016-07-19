5ee551d65a52460386491cbffc26941986d3c06e
maira1001001 <maira.diaz@protonmail.com>
2016-07-19 12:54:31 -0300

    arreglo errors en modelos

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,7 +2,7 @@ class Participation < ActiveRecord::Base
   belongs_to :councilor, -> { councilor }, class_name: 'User', foreign_key: 'councilor_id'
   belongs_to :district
   belongs_to :political_party
-  has_many   :projects
+  has_many   :projects, dependent: :destroy, autosave: true
   has_many   :collaborators, -> { collaborator }, class_name: 'User', foreign_key: 'collaborator_id'
 
   validates :start_date, :in_function, :district_id, :political_party_id, presence: true
--- app/models/project.rb
+++ app/models/project.rb
@@ -3,16 +3,15 @@ class Project < ActiveRecord::Base
   belongs_to :district
   belongs_to :political_party
   belongs_to :participation, autosave: true
-  has_many   :project_files
-
-  before_save :update_attributes
+  has_many   :project_files, dependent: :destroy, autosave: true
 
   enum project_type: { ordinance: 0, decree: 1, resolution: 2, communication: 3, document: 4 }
   enum category: { salud: 0, educacion: 1, desarrollo_social: 2 }
 
   validates :title, :description, :category, :project_type, presence: true
 #  validates_associated :project_files
-  accepts_nested_attributes_for :project_files, allow_destroy: true
+
+accepts_nested_attributes_for :project_files, allow_destroy: true
 
   scope :all_from_current_participation, lambda { |participation| Participation.find(participation).projects }
 
@@ -20,11 +19,6 @@ class Project < ActiveRecord::Base
 
   def to_s
     title
-  end
-
-  def update_attributes
-    district = participation.district
-    political_party = participation.political_party
   end
 
   def not_visible
